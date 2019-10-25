4793d522238a6c02fb8ac30feb75bda484d044b6
jehyeon <hyeon.rot@gmail.com>
2019-10-25 09:55:10 +0900

    Remove lib from .gitignore

---
+++ joygarden.kidsBi/code/lib/category.js
@@ -0,0 +1,1 @@
+exports.list = ['Aesop', 'Bed', 'Car', 'Christmas', 'Collection', 'Constellation', 'Dinosaur', 'English', 'Korean tradition', 'Musical', 'Princess', 'Science', 'Shadow', 'Traditional'];
