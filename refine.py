b0a5593f0e6f2b6bb76e8131b40457eb1907ddc2
hyeon62 <hyeon.rot@gmail.com>
2019-10-27 19:22:55 +0900

    Modify wrongs

--- refine.py
+++ refine.py
@@ -11,18 +11,18 @@ def main():
     for videoId in jsonDatas:
       # get url using youtube-dl
       command = 'sudo youtube-dl -f best -g --no-check-certificate http://www.youtube.com/watch?v={0}'.format(videoId)
-      url = check_output(command.split()).decode('utf-8')
+      url = check_output(command.split()).decode('utf-8').replace('\n', '')
       modified[videoId]['url'] = url
       if (len(url) > 254):
-        print(len(url))
+        print('url: ' + str(len(url)))
 
       # get thumbnail image using youtube-dl
       command = 'sudo youtube-dl --write-thumbnail --no-check-certificate --verbose http://www.youtube.com/watch?v={0} --skip-download --get-thumbnail'.format(videoId)
       # call(command.split(), shell=False)
-      thumbnail = check_output(command.split()).decode('utf-8')
+      thumbnail = check_output(command.split()).decode('utf-8').split('\n')[1].replace('\n', '')
       modified[videoId]['thumbnail'] = thumbnail
       if (len(thumbnail) > 254):
-        print(len(thumbnail))
+        print('url: ' + str(len(thumbnail)))
 
       modified[videoId]['like'] = float(modified[videoId]['like'].split('%')[0])
       if modified[videoId]['like'] > 80:
@@ -30,8 +30,8 @@ def main():
       del modified[videoId]['dislike']
 
   # save
-  with open('datas.json', 'r', encoding='utf8') as data:
-    json.dumps(modified, datas, ensure_ascii=False, indent=2)
+  with open('datas.json', 'w', encoding='utf8') as datas:
+    json.dump(modified, datas, ensure_ascii=False, indent=2)
 
 if __name__ == '__main__':
   main()
