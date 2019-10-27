92ede87354c11d673f5ec2ffc877563602363877
hyeon62 <hyeon.rot@gmail.com>
2019-10-28 01:03:48 +0900

    Add request.py

---
+++ request.py
@@ -0,0 +1,39 @@
+import requests
+import json
+
+def main():
+  url = 'http://127.0.0.1:5000/api/videos/'
+  headers = {
+    'Content-Type': 'application/json'
+  }
+
+  with open('datas.json', 'r', encoding='utf8') as f:
+    datas = json.load(f)
+
+    for videoId in datas.keys():
+      
+      data = {
+        'title': datas[videoId]['title'],
+        'videoId': videoId,
+        'source': datas[videoId]['source'],
+        'videoThumbnail': datas[videoId]['thumbnail'],
+        'callCount': 0,
+        'likeRatio': int(datas[videoId]['like']),
+        'viewCount': datas[videoId]['viewCount'],
+        'categories': ','.join(datas[videoId]['categories']), # temp
+        'videoFile': datas[videoId]['url'],
+        'funSticker': 0,
+        'notFunSticker': 0,
+        'updateDate': '2019-10-28T00:50'
+      }
+
+      # print()
+      # print(json.dumps(data))
+      res = requests.post(url=url, headers=headers, data=json.dumps(data))
+      print(res)
+      break
+      # requests.post(url=url, headers=headers, data=data)
+
+if __name__ == '__main__':
+  main()
+
