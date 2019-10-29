661de38c3b492d38bddb5dcd5eb4a3802ae9df2b
soohoan <sooho.an@lionbridge.com>
2019-10-29 16:21:42 +0900

    added quizprogress

--- joygarden.kidsBi/code/UpdateProgress.js
+++ joygarden.kidsBi/code/UpdateProgress.js
@@ -6,6 +6,22 @@ module.exports.function = function updateProgress (quizProgress, answer) {
 
   if (quizProgress.quizInfos[quizProgress.index].answer === answers.indexOf(answer.valueOf())) {
     quizProgress.result[quizProgress.index] = 'true';
+    quizProgress.quizInfos[quizProgress.index].answerResult = 'true';
+
+  }
+
+  for (var i = 0; i < answers.length; i++) {
+    //console.log(String(quizProgress.quizInfos[quizProgress.index].options[i]).slice(3));
+    if (String(quizProgress.quizInfos[quizProgress.index].options[i]).slice(3) == answer) {
+      //입력받은 답과 옵션에서의 답이 같으면 그 옵션의 첫번째 값(A,B,C,D)을 answer_name에 저장
+      var answer_name = String(quizProgress.quizInfos[quizProgress.index].options[i]).slice(0, 1);
+    }
+  }
+  //answer)name의 값과 quizInfos[quizProgress.index]의 값이 같은지 확인 
+  if (answers[quizProgress.quizInfos[quizProgress.index].answer] === answer_name) {
+    quizProgress.result[quizProgress.index] = 'true';
+    quizProgress.quizInfos[quizProgress.index].answerResult = 'true';
+    //console.log(quizProgress.result[quizProgress.index])
   }
 
   if (quizProgress.quizInfos.length === quizProgress.index + 1) {
