53616aa7c5332877bfd0c0fce03cc03c04c445b4
soohoan <sooho.an@lionbridge.com>
2019-10-30 14:00:26 +0900

    updated StartQuiz

--- joygarden.kidsBi/code/UpdateProgress.js
+++ joygarden.kidsBi/code/UpdateProgress.js
@@ -20,25 +20,25 @@ module.exports.function = function updateProgress(quizProgress, answer) {
   //답을 직접 받아오는 경우
   else if (answer.indexOf(':') == -1) {
     for (var i = 0; i < answers.length; i++) {
-      console.log(String(quizProgress.quizInfos[quizProgress.index].options[i]).slice(2));
+      //console.log(String(quizProgress.quizInfos[quizProgress.index].options[i]).slice(2));
       if (String(quizProgress.quizInfos[quizProgress.index].options[i]).slice(2) == answer) {
         //입력받은 답과 옵션에서의 답이 같으면 그 옵션의 첫번째 값(A,B,C,D)을 answer_name에 저장
         var answer_name = String(quizProgress.quizInfos[quizProgress.index].options[i]).slice(0, 1);
-        //console.log(answer_name);
+        console.log(answer_name);
       }
     }
-    //answer)name의 값과 quizInfos[quizProgress.index]의 값이 같은지 확인 
+    //answer_name의 값과 quizInfos[quizProgress.index]의 값이 같은지 확인 
     if (answers[quizProgress.quizInfos[quizProgress.index].answer] === answer_name) {
       quizProgress.result[quizProgress.index] = 'true';
       quizProgress.quizInfos[quizProgress.index].answerResult = 'true';
       //console.log(quizProgress.result[quizProgress.index])
     }
   }
-  //intet인 값을 받아오는 경우
+  //intetn인 값을 받아오는 경우
   else {
     for (var i = 0; i < answers.length; i++) {
       var answer = String(answer).slice(0, 1); 
-      console.log(String(quizProgress.quizInfos[quizProgress.index].options[i]).slice(0,1));
+      //console.log(String(quizProgress.quizInfos[quizProgress.index].options[i]).slice(0,1));
       if (String(quizProgress.quizInfos[quizProgress.index].options[i]).slice(0,1) == answer) {
         //입력받은 답과 옵션에서의 답이 같으면 그 옵션의 첫번째 값(A,B,C,D)을 answer_name에 저장
         var answer_name = String(quizProgress.quizInfos[quizProgress.index].options[i]).slice(0, 1);
