7dfd5217764e3c17fe17dfec12e8170fbd26b80a
soohoan <sooho.an@lionbridge.com>
2019-10-31 20:54:35 +0900

    added Validtion When No VideoInfo

--- joygarden.kidsBi/code/OpenVideoInYoutube.js
+++ joygarden.kidsBi/code/OpenVideoInYoutube.js
@@ -1,3 +1,8 @@
+var fail = require('fail');
+
 module.exports.function = function openVideoInYoutube (videoInfo) {
+  if (!videoInfo) {
+    throw fail.checkedError('No video info', 'NoVideoInfo');
+  }
   return "https://www.youtube.com/watch?v=" + videoInfo.videoId;
 }
